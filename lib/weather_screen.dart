c4b63cb9b4938d522fc869b7f1955ab24eb0eee0
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2025-03-10 14:12:34 +0100

    The Weather App is a sleek, user-friendly mobile application designed to provide real-time weather updates and forecasts. Built using Flutter, a powerful and versatile framework by Google, the app offers a seamless and responsive experience across both iOS and Android platforms.

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -27,28 +27,58 @@ class _WeatherScreenState extends State<WeatherScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          //main card
-          const Placeholder(
-            fallbackHeight: 250,
-            //child: const Text('Main Card'),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // weather forecast card
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // sweather additonal information
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            //main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        '300 °F',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      const Icon(
+                        Icons.cloud,
+                        size: 60,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      const Text('Rain',style: TextStyle(fontSize: 20),),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // weather forecast card
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // sweather additonal information
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
